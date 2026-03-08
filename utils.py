954cf28dd4798e39fdbabfa6fb394a9d71d1653a
Kurt Shubert <kpshubert@kpshubert.com>
2026-03-08 14:16:15 -0500

    20260308 Add Employee Filtering/Fix Issues
    Add Department Filter to the Employee List
    
    Fix various issues found in testing.

--- utils.py
+++ utils.py
@@ -6,3 +6,13 @@ def employees_in_department(DepartmentID):
         cursor.execute("SELECT COUNT(*) FROM tEM_Employee WHERE tEM_Employee.DepartmentID =" + str(DepartmentID))
         result = cursor.fetchall()
         return result[0][0]
+
+def prepend_if_not_empty(operation_string, prepend_string):
+    return_value = ''
+
+    if operation_string == '':
+        return_value = ''
+    else:
+        return_value = f'{prepend_string}{operation_string}'
+
+    return return_value
